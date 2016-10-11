27600bdbe4c8d794a2a866c52b1cb62a9f69caec
khalil <khalil@khalil-PC>
2016-10-11 14:22:31 +0100

    First Commit

---
+++ src/beans/Client.java
@@ -0,0 +1,44 @@
+package beans;
+
+public class Client {
+
+	int id ;
+	String nom;
+	String prenon;
+	String adress;
+	String societe;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenon() {
+		return prenon;
+	}
+	public void setPrenon(String prenon) {
+		this.prenon = prenon;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getSociete() {
+		return societe;
+	}
+	public void setSociete(String societe) {
+		this.societe = societe;
+	}
+	
+	
+	
+	
+}
